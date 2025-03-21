de67fd82f58a40ca65e4bb094b3ddc6e80454516
fpdellazari <fpdellazari@outlook.com>
2025-03-20 23:40:02 -0300

    [refactor] Limpeza de usings desnecessários.

--- FelipeNoRepositoryApiTemplate.Domain/Models/DTOs/UpdateCustomerDTO.cs
+++ FelipeNoRepositoryApiTemplate.Domain/Models/DTOs/UpdateCustomerDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FelipeNoRepositoryApiTemplate.Domain.Models.DTOs;
+﻿namespace FelipeNoRepositoryApiTemplate.Domain.Models.DTOs;
 
 public record UpdateCustomerDTO(string Name, int Age, string? Email);
 
